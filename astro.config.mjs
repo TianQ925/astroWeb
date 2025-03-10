519c289eb7623d4d9d71457baad14aa49ea71e9f
王美人 <yiyi@wangmeirendeMacBook-Pro.local>
2025-03-10 19:07:52 +0100

    Set up GitHub Actions for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import rehypeExternalLinks from 'rehype-external-links'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://TianQ925.github.io',
-  base: '/astroWeb/',
+  site: 'https://tianq925.github.io',
+  base: '/astroWeb',
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
